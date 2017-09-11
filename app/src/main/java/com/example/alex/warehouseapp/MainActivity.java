558d28db2444b85e88d9b10ec6973ed749678113
Alex Merz <akm29@studnets.waikato.ac.nz>
2017-09-11 18:39:59 +1200

    Implemented department spinner for deals

--- app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
+++ app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.alex.warehouseapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup spinner
+        Spinner departmentSpinner = (Spinner)findViewById(R.id.departmentSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.departments, R.layout.support_simple_spinner_dropdown_item
+                );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        departmentSpinner.setAdapter(adapter);
     }
 }
