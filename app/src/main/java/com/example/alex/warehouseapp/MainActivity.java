c6d7923cb8393cf28298a82aab5b9b9bac005ca4
Alex Merz <akm29@studnets.waikato.ac.nz>
2017-09-11 19:11:29 +1200

    Searching item sends user to item activity
    Searched item is also sent to activity

--- app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
+++ app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.alex.warehouseapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +25,20 @@ public class MainActivity extends AppCompatActivity {
                 );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         departmentSpinner.setAdapter(adapter);
+
+        //Setup search edit text to launch item activity
+        final EditText searchItem = (EditText)findViewById(R.id.searchItem);
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        //Launch on button press
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get text and send with intent
+                String value = searchItem.getText().toString();
+                Intent nextActivity = new Intent(getBaseContext(), ItemsActivity.class);
+                nextActivity.putExtra("search", value);
+                startActivity(nextActivity);
+            }
+        });
     }
 }
