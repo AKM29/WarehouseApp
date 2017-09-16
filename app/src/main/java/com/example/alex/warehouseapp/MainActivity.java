52e10aadc1d6124d65424f78a15dc7f0f7f1d598
Alex Merz <akm29@studnets.waikato.ac.nz>
2017-09-16 16:56:04 +1200

    App now gets users last known location

--- app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
+++ app/src/main/java/com/example/alex/warehouseapp/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.alex.warehouseapp;
 
+import android.*;
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -11,12 +17,46 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedClient;
+    private Location clientLocation;
+
+
+    private static final int FINE_LOCATION_PERMISSION = 1;
+    private boolean allowFineLocation = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup client
+        fusedClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+
+            //Get last known location
+            fusedClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    clientLocation = location;
+                }
+            });
+        }
+        //If not allowed, ask for permission
+        else {
+            Toast.makeText(getBaseContext(), "Not allowed", Toast.LENGTH_SHORT).show();
+        }
+
 
         //Setup spinner
         Spinner departmentSpinner = (Spinner)findViewById(R.id.departmentSpinner);
@@ -51,5 +91,18 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    //Handle permission requests
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode) {
+            case FINE_LOCATION_PERMISSION: {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    allowFineLocation = true;
+                }
+                return;
+            }
+        }
     }
 }
