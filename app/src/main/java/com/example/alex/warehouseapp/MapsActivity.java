9dca74e78cffb15e34ea211c34fd93da6a23fd7e
“Sarah <Lulwah_alduraiei@outlook.com>
2017-09-25 17:27:20 +1300

    Adding the basic map with kml layer and search option

---
+++ app/src/main/java/com/example/alex/warehouseapp/MapsActivity.java
@@ -0,0 +1,118 @@
+package com.example.alex.warehouseapp;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.kml.KmlLayer;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by Sarah & Lulwah.
+ */
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(googleServicesAvailable()){
+            Toast.makeText(this,"Perfect",Toast.LENGTH_LONG).show();
+            setContentView(R.layout.activity_maps);
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+
+
+
+        }
+
+
+
+    }
+
+    public void search(View v){
+        Intent intent = new Intent(this, Search.class);
+        startActivity(intent);
+    }
+
+    //
+    public void kmlLAyer(){
+        try {
+            InputStream ins = getResources().openRawResource(
+                    getResources().getIdentifier("map",
+                            "raw", getPackageName()));
+
+            KmlLayer kmlLayer = new KmlLayer(mMap, ins, getApplicationContext());
+
+            kmlLayer.addLayerToMap();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean googleServicesAvailable(){
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if(isAvailable== ConnectionResult.SUCCESS){
+            return true;
+        }else if(api.isUserResolvableError(isAvailable)){
+            Dialog dialog = api.getErrorDialog(this, isAvailable,0);
+            dialog.show();
+
+        }else {
+            Toast.makeText(this,"Cannot connect to play services",Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Add a marker in warehouse and move the camera
+        LatLng warehouse = new LatLng(-37.789339, 175.308230);
+        mMap.addMarker(new MarkerOptions().position(warehouse).title("Marker in warehouse"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(warehouse,20));
+        // Zoom in, animating the camera.
+        googleMap.animateCamera(CameraUpdateFactory.zoomIn());
+        // Zoom out to zoom level 10, animating with a duration of 2 seconds.
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
+
+        kmlLAyer();
+    }
+
+}
