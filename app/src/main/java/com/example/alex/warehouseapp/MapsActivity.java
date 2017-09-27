5b96f04810428d09a0fc4c4c8f4a939774a9a716
“Sarah <Lulwah_alduraiei@outlook.com>
2017-09-27 15:35:42 +1300

    Adding the KML layer on the map with marks on each section (by Sarah & Lulwah)

--- app/src/main/java/com/example/alex/warehouseapp/MapsActivity.java
+++ app/src/main/java/com/example/alex/warehouseapp/MapsActivity.java
@@ -2,8 +2,11 @@ package com.example.alex.warehouseapp;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,23 +17,29 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.data.kml.KmlLayer;
+import com.google.maps.android.ui.IconGenerator;
 
 import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.IOException;
 import java.io.InputStream;
 
+import static android.graphics.Typeface.BOLD;
+import static android.graphics.Typeface.ITALIC;
+import static android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE;
+
+
 /**
  * Created by Sarah & Lulwah.
  */
-
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    Button btn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,23 +51,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.map);
             mapFragment.getMapAsync(this);
-        }
-    }
 
+
+
+
+        }
+
+
+
+    }
+    //opening the search activity
     public void search(View v){
         Intent intent = new Intent(this, Search.class);
         startActivity(intent);
     }
 
-    //
+    //adding the kml layer to the map
     public void kmlLAyer(){
         try {
             InputStream ins = getResources().openRawResource(
-                    getResources().getIdentifier("map", "raw", getPackageName()));
+                    getResources().getIdentifier("test",
+                            "raw", getPackageName()));
 
             KmlLayer kmlLayer = new KmlLayer(mMap, ins, getApplicationContext());
 
             kmlLayer.addLayerToMap();
+
         } catch (IOException e) {
             e.printStackTrace();
         } catch (XmlPullParserException e) {
@@ -66,7 +84,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
     }
-
+    //Checking if google play service is available
     public boolean googleServicesAvailable(){
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
@@ -102,8 +120,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         googleMap.animateCamera(CameraUpdateFactory.zoomIn());
         // Zoom out to zoom level 10, animating with a duration of 2 seconds.
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(20), 2000, null);
-
+        //calling the kml layer method
+        startDemo();
         kmlLAyer();
+
     }
+
+
+    protected void startDemo() {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-37.789339, 175.3082300), 10));
+
+        IconGenerator iconFactory = new IconGenerator(this);
+        addIcon(iconFactory, "Default", new LatLng(-37.789339, 175.308230));
+
+        iconFactory.setColor(Color.CYAN);
+        addIcon(iconFactory, "warehouse", new LatLng(-37.788427,175.307828));
+
+        iconFactory.setRotation(90);
+        iconFactory.setStyle(IconGenerator.STYLE_RED);
+        addIcon(iconFactory, "warehouse", new LatLng(-37.788608,175.308181));
+
+        iconFactory.setContentRotation(-90);
+        iconFactory.setStyle(IconGenerator.STYLE_PURPLE);
+        addIcon(iconFactory, "warehouse", new LatLng(-37.788641,175.307907));
+
+        iconFactory.setRotation(0);
+        iconFactory.setContentRotation(90);
+        iconFactory.setStyle(IconGenerator.STYLE_GREEN);
+        addIcon(iconFactory, "warehouse", new LatLng(-37.789088,175.308354));
+
+        iconFactory.setRotation(0);
+        iconFactory.setContentRotation(0);
+        iconFactory.setStyle(IconGenerator.STYLE_ORANGE);
+        addIcon(iconFactory, makeCharSequence(), new LatLng(-37.789339, 175.3082300));
+    }
+
+    private void addIcon(IconGenerator iconFactory, CharSequence text, LatLng position) {
+        MarkerOptions markerOptions = new MarkerOptions().
+                icon(BitmapDescriptorFactory.fromBitmap(iconFactory.makeIcon(text))).
+                position(position).
+                anchor(iconFactory.getAnchorU(), iconFactory.getAnchorV());
+
+        mMap.addMarker(markerOptions);
+    }
+
+    private CharSequence makeCharSequence() {
+        String prefix = "Mixing ";
+        String suffix = "different fonts";
+        String sequence = prefix + suffix;
+        SpannableStringBuilder ssb = new SpannableStringBuilder(sequence);
+        ssb.setSpan(new StyleSpan(ITALIC), 0, prefix.length(), SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(new StyleSpan(BOLD), prefix.length(), sequence.length(), SPAN_EXCLUSIVE_EXCLUSIVE);
+        return ssb;
+    }
+
 
 }
