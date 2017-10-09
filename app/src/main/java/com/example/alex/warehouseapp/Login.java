3f287b68f379830c6c4400e8e0f0aaa5dea3024e
DESKTOP-NJOKSJM\WardB <Wardbeehre@gmail.com>
2017-10-10 12:06:32 +1300

    added login activity

---
+++ app/src/main/java/com/example/alex/warehouseapp/Login.java
@@ -0,0 +1,13 @@
+package com.example.alex.warehouseapp;
+
+import android.os.Bundle;
+
+/**
+ * Created by WardB on 10/10/2017.
+ */
+
+public class Login {
+
+    protected void onCreate(Bundle savedInstanceState) {
+    }
+}
